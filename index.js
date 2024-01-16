a23f2e2d041c871d91b806cacdca073daa584bf3
@Infer <96887546+Infernite@users.noreply.github.com>
2024-01-16 19:44:13 +0630

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-var http = require("http");
-http.createServer((function(e, o) {
-	o.write("I'm alive"), o.end()
-})).listen(8080);
-const {
-	Client: Client
-} = require("discord.js-selfbot-v13"), fs = require("fs"), client = new Client({
-	checkUpdate: !1
-}), rawConfig = fs.readFileSync("config.json"), config = JSON.parse(rawConfig);
-
-function sendSlashCommand(n, e, s) {
-	const o = client.channels.cache.get(n);
-	o ? o.sendSlash(e, s) : console.error(`Channel with ID ${n} not found.`)
-}
+const http = require("http"),
+	{
+		Client: Client
+	} = require("discord.js-selfbot-v13"),
+	fs = require("fs"),
+	client = new Client({
+		checkUpdate: !1
+	}),
+	rawConfig = fs.readFileSync("config.json"),
+	config = JSON.parse(rawConfig),
+	sendSlashCommand = (e, n, o) => {
+		const a = client.channels.cache.get(e);
+		a ? a.sendSlash(n, o) : console.error(`Channel with ID ${e} not found.`)
+	};
 client.on("ready", (async () => {
 	console.log(`${client.user.username} is ready!`), sendSlashCommand(config.channelID, "490707751832649738", "work"), setTimeout((() => {
 		sendSlashCommand(config.channelID, "490707751832649738", "tips")
@@ -24,4 +24,9 @@ client.on("ready", (async () => {
 	}), 247e3), setInterval((() => {
 		sendSlashCommand(config.channelID, "490707751832649738", "overtime")
 	}), 1807e3)
+})), client.on("interactionCreate", (e => {
+	e.isCommand() && ("work" === e.commandName ? console.log("Work command triggered.") : "tips" === e.commandName ? console.log("Tips command triggered.") : "overtime" === e.commandName && console.log("Overtime command triggered."), e.reply({
+		content: "",
+		ephemeral: !0
+	}))
 })), client.login(process.env.token);
