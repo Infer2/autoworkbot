ac689c8b60d5f2f7d8529e0b080ba3dc424d5206
@Infer <96887546+Infernite@users.noreply.github.com>
2024-01-17 00:24:00 +0630

    Update index.js

--- index.js
+++ index.js
@@ -1,40 +1,29 @@
 const http = require("http"),
-  { Client } = require("discord.js-selfbot-v13"),
-  fs = require("fs"),
-  client = new Client({
-    checkUpdate: false,
-  }),
-  rawConfig = fs.readFileSync("config.json"),
-  config = JSON.parse(rawConfig),
-  sendSlashCommand = (channelId, commandId, commandName) => {
-    const channel = client.channels.cache.get(channelId);
-    if (channel) {
-      setTimeout(() => {
-        channel.sendSlash(commandId, commandName);
-      }, 1000); // Add a 1-second delay before sending the command
-    } else {
-      console.error(`Channel with ID ${channelId} not found.`);
-    }
-  };
-
-client.on("ready", async () => {
-  console.log(`${client.user.username} is ready!`);
-  sendSlashCommand(config.channelID, "490707751832649738", "work");
-  setTimeout(() => {
-    sendSlashCommand(config.channelID, "490707751832649738", "tips");
-  }, 1000);
-  setTimeout(() => {
-    sendSlashCommand(config.channelID, "490707751832649738", "overtime");
-  }, 2000);
-  setInterval(() => {
-    sendSlashCommand(config.channelID, "490707751832649738", "work");
-  }, 607000);
-  setInterval(() => {
-    sendSlashCommand(config.channelID, "490707751832649738", "tips");
-  }, 307000);
-  setInterval(() => {
-    sendSlashCommand(config.channelID, "490707751832649738", "overtime");
-  }, 1807000);
-});
-
-client.login(process.env.token);
+	{
+		Client: Client
+	} = require("discord.js-selfbot-v13"),
+	fs = require("fs"),
+	client = new Client({
+		checkUpdate: !1
+	}),
+	sendSlashCommand = (e, n, t) => {
+		const s = client.channels.cache.get(e);
+		s ? setTimeout((() => {
+			s.sendSlash(n, t)
+		}), 1e3) : console.error(`Channel with ID ${e} not found.`)
+	};
+client.on("ready", (async () => {
+	console.log(`${client.user.username} is ready!`), sendSlashCommand(config.channelID, "490707751832649738", "work"), setTimeout((() => {
+		sendSlashCommand(config.channelID, "490707751832649738", "tips")
+	}), 1e3), setTimeout((() => {
+		sendSlashCommand(config.channelID, "490707751832649738", "overtime")
+	}), 2e3), setInterval((() => {
+		sendSlashCommand(config.channelID, "490707751832649738", "work")
+	}), 607e3), setInterval((() => {
+		sendSlashCommand(config.channelID, "490707751832649738", "tips")
+	}), 307e3), setInterval((() => {
+		sendSlashCommand(config.channelID, "490707751832649738", "overtime")
+	}), 1807e3)
+})), client.login(process.env.token), http.createServer((function(e, n) {
+	n.write("I'm alive"), n.end()
+})).listen(8080);
