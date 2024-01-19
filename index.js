0e7d2e22e7d7dca6bf3bb14b425ff3f9199b5004
@Infer <96887546+Infernite@users.noreply.github.com>
2024-01-20 03:09:47 +0630

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
 const http = require("http"),
   { Client } = require("discord.js-selfbot-v13"),
-  fs = require("fs"),
   client = new Client({
     checkUpdate: false,
   });
-
-// Read config.json file
-const config = JSON.parse(fs.readFileSync("config.json", "utf8"));
 
 const sendSlashCommand = (channelId, command, text, delay) => {
   const channel = client.channels.cache.get(channelId);
@@ -23,13 +19,13 @@ client.on("ready", async () => {
   console.log(`${client.user.username} is ready!`);
   
   // Initial set of slash commands with 0.8s delay between each
-  sendSlashCommand(config.channelID, "490707751832649738", "work", 900);
-  sendSlashCommand(config.channelID, "490707751832649738", "tips", 1800);
-  sendSlashCommand(config.channelID, "490707751832649738", "overtime", 2700);
+  sendSlashCommand(process.env.channel, "490707751832649738", "work", 900);
+  sendSlashCommand(process.env.channel, "490707751832649738", "tips", 1800);
+ // sendSlashCommand(process.env.channel, "490707751832649738", "overtime", 2700);
   
   // Set timers for each command
-  let workTimer = 607000;
-  let tipsTimer = 307000;
+  let workTimer = 547000;
+  let tipsTimer = 247000;
   let overtimeTimer = 1807000;
   
   // Function to send slash command and reset its timer
@@ -43,7 +39,7 @@ client.on("ready", async () => {
   // Start timers for each command
   sendAndResetTimer("work", workTimer);
   sendAndResetTimer("tips", tipsTimer);
-  sendAndResetTimer("overtime", overtimeTimer);
+//  sendAndResetTimer("overtime", overtimeTimer);
 });
 
 client.login(process.env.token);
