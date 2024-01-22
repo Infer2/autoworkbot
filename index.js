3899a23348433dad326bc3c099f88cc3dabfbebc
@Infer <96887546+Infernite@users.noreply.github.com>
2024-01-22 10:22:28 +0630

    Update index.js

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const http = require("http"),
 	client = new Client({
 		checkUpdate: !1
 	}),
-	sendSlashCommand = (e, n, t, s) => {
+	sendSlashCommand = (e, n, s, t) => {
 		const o = client.channels.cache.get(e);
 		o ? setTimeout((() => {
-			o.sendSlash(n, t)
-		}), s) : console.error(`Channel with ID ${e} not found.`)
+			o.sendSlash(n, s).catch((e => console.error(`Error sending slash command: ${e}`)))
+		}), t) : console.error(`Channel with ID ${e} not found.`)
 	};
 client.on("ready", (async () => {
 	console.log(`${client.user.username} is ready!`);
@@ -18,7 +18,7 @@ client.on("ready", (async () => {
 			sendSlashCommand(process.env.channel, "490707751832649738", e, 0)
 		}), n)
 	};
-	e("work", 547e3), e("tips", 247e3), e("overtime", 18075e2)
+	e("work", 548e3), e("tips", 248e3), e("overtime", 1808e3)
 })), client.login(process.env.token), http.createServer(((e, n) => {
 	n.write("I'm alive"), n.end()
 })).listen(8080);
