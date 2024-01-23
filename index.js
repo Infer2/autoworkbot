2d807be367ec4cb48c2a3fa6be7e5cc6b95db114
@Infer <96887546+Infernite@users.noreply.github.com>
2024-01-24 00:59:39 +0630

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,24 @@
-const http = require("http");
-const { Client } = require("discord.js-infer");
-
-const client = new Client({
-  checkUpdate: false,
-});
-
-const sendSlashCommand = (channelId, command, payload, delay) => {
-  const channel = client.channels.cache.get(channelId);
-  if (channel) {
-    setTimeout(() => {
-      channel.sendSlash(command, payload)
-        .catch((error) => console.error(`Error sending slash command: ${error}`));
-    }, delay);
-  } else {
-    console.error(`Channel with ID ${channelId} not found.`);
-  }
-};
-
-client.on("ready", async () => {
-  console.log(`${client.user.username} is ready!`);
-  const sendRepeatedCommands = (command, interval) => {
-    sendSlashCommand(process.env.channel, "490707751832649738", command, 0);
-    setInterval(() => {
-      sendSlashCommand(process.env.channel, "490707751832649738", command, 0);
-    }, interval);
-  };
-
-  sendRepeatedCommands("work", 548e3);
-  sendRepeatedCommands("tips", 248e3);
-  sendRepeatedCommands("overtime", 1808e3);
-});
-
-client.login(process.env.token);
-
-http.createServer((req, res) => {
-  res.write("I'm alive");
-  res.end();
-}).listen(8080);
+const http = require("http"),
+	{
+		Client: Client
+	} = require("discord.js-infer"),
+	client = new Client({
+		checkUpdate: !1
+	}),
+	sendSlashCommand = (e, n, s, t) => {
+		const o = client.channels.cache.get(e);
+		o ? setTimeout((() => {
+			o.sendSlash(n, s).catch((e => console.error(`Error sending slash command: ${e}`)))
+		}), t) : console.error(`Channel with ID ${e} not found.`)
+	};
+client.on("ready", (async () => {
+	console.log(`${client.user.username} is ready!`);
+	const e = (e, n) => {
+		sendSlashCommand(process.env.channel, "490707751832649738", e, 0), setInterval((() => {
+			sendSlashCommand(process.env.channel, "490707751832649738", e, 0)
+		}), n)
+	};
+	e("work", 548e3), e("tips", 248e3), e("overtime", 1808e3)
+})), client.login(process.env.token), http.createServer(((e, n) => {
+	n.write("I'm alive"), n.end()
+})).listen(8080);
